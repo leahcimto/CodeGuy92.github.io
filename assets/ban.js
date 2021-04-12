7ca6ac3601b713fab025a951645442da9c5e458e
CodeGuy92 <79767244+CodeGuy92@users.noreply.github.com>
2021-04-12 18:10:17 +0300

    Fix blank reason message

--- assets/ban.js
+++ assets/ban.js
@@ -1,4 +1,8 @@
+if (location.hash.length > 0) {
 var reason = location.hash;
 var fixed = reason.slice(1, 999);
 var converted = fixed.replaceAll("%20", " ")
 document.getElementById("reason").innerText = converted;
+}else{
+document.getElementById("reason").innerText = "No reason provided.";
+}
