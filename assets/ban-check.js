16aa738930989bca8c8948687f624325df0eda87
CodeGuy92 <79767244+CodeGuy92@users.noreply.github.com>
2021-04-12 18:24:27 +0300

    Create ban-check.js

---
+++ assets/ban-check.js
@@ -0,0 +1,12 @@
+function reload() {
+var username = document.getElementById("usename");
+location.replace(window.location.href + "#" + username);
+window.location.reload();
+}
+
+if (location.hash == 'LankyBox01') {
+location.replace(`https://codeguy92.github.io/website/ban#Sending too many requests.`);
+}
+if (location.hash == 'Jeffalo') {
+location.replace(`https://codeguy92.github.io/website/ban#Being too advanced at coding. I don't know if that's even a valid ban reason, LOL!`);
+}
