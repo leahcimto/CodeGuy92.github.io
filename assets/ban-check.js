e70c3cc67092d09ccc60023fa9aae83c3715adca
CodeGuy92 <79767244+CodeGuy92@users.noreply.github.com>
2021-04-13 00:36:08 +0300

    Update ban-check.js

--- assets/ban-check.js
+++ assets/ban-check.js
@@ -2,8 +2,16 @@ function reload() {
 var username = document.getElementById("usename").value;
 location.replace(window.location.href + "#" + username);
 window.location.reload();
+
+document.cookie = "user=lankybox01";
 }
 
-if (location.hash == '#LankyBox01') {
-location.replace(`https://codeguy92.github.io/website/ban#You were banned for testing purposes.`);
+function onload() {
+  var cookie1 = document.cookie;
+  var username1 = cookie1.split('=')[1];
+  location.replace(window.location.href + "#" + username);
+  
+  if (location.hash == '#LankyBox01') {
+    location.replace(`https://lankybox01.leahcimto.com/website/ban#You were banned for testing purposes.`);
+  }
 }
